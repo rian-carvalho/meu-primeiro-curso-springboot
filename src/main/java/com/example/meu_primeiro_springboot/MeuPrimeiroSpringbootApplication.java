88e8d0249a97178a8a315222b5435ba71edfbd18
Rian Carvalho Galcez <riancarvalhogalcez@gmail.com>
2026-03-18 14:39:47 -0300

    Update MeuPrimeiroSpringbootApplication.java

--- src/main/java/com/example/meu_primeiro_springboot/MeuPrimeiroSpringbootApplication.java
+++ src/main/java/com/example/meu_primeiro_springboot/MeuPrimeiroSpringbootApplication.java
@@ -3,6 +3,12 @@ package com.example.meu_primeiro_springboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Projeto: Meu Primeiro Spring Boot
+ * Autor: Rian Carvalho
+ * Data: 18/03/2026
+ * Descrição: API REST desenvolvida utilizando Spring Boot para fins de estudo e prática
+ */
 @SpringBootApplication
 public class MeuPrimeiroSpringbootApplication {
 
